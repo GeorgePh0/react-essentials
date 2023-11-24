da9b1c60056e33e6622a2285cb32e70ca83c208d
GeorgePh0 <george.philip0020@gmail.com>
2023-11-24 11:54:19 +0000

    Add Props for Child and Parent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,12 +10,15 @@ import EventsFunctional from './components/EventsFunctional';
 import EventBinding from './components/EventBinding';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsChild from './components/MethodsAsPropsChild';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
       <StatefulGreeting greeting="I am a stateful class component!"/>
       <StatefulGreetingWithCallback />
@@ -25,6 +28,8 @@ function App() {
       <EventBinding />
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true} />
+      <NestingComponents /> */}
+      <MethodsAsPropsParent />
     </div>
   );
 }
