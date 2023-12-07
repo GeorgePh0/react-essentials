118fbd806c8474c951127845ccc3f6b85ef9cae1
GeorgePh0 <george.philip0020@gmail.com>
2023-12-07 11:28:55 +0000

    ComponentWillUnmount()

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,6 +16,7 @@ import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 import RenderingLists from './components/RenderingLists';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
+import LifeCyclesCWU from './components/LifeCyclesCWU';
 
 
 function App() {
@@ -36,6 +37,7 @@ function App() {
       <RenderingLists /> */}
       <LifeCyclesCDM />
       <LifeCyclesCDU />
+      <LifeCyclesCWU />
     </div>
   );
 }
